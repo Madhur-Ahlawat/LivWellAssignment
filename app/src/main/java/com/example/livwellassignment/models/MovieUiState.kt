aebd18893b7ee8876b8890772ff94103065459bd
52408391 <madhur.ahlawat@conduent.com>
2025-08-12 19:31:21 +0530

    App Integrity & Security Checks

--- app/src/main/java/com/example/livwellassignment/models/MovieUiState.kt
+++
@@ -1,9 +0,0 @@
-package com.example.livwellassignment.models
-
-import androidx.paging.PagingData
-
-sealed class MovieUiState {
-    object Loading : MovieUiState()
-    data class Success(val movies: PagingData<MovieListItem>) : MovieUiState()
-    data class Error(val message: String) : MovieUiState()
-}
--- app/src/main/java/com/example/livwellassignment/models/enums/FocusedFieldEnum.kt
+++
@@ -1,5 +0,0 @@
-package com.example.livwellassignment.models.enums
-
-enum class FocusedFieldEnum {
-    NONE, CARD, EXPIRY
-}
